25f267eda5a2b75249568afc126776e46dc625d0
Agwebberley <44350096+Agwebberley@users.noreply.github.com>
2020-03-04 14:18:26 -0800

    Update cryption2.py

--- cryption2.py
+++ cryption2.py
@@ -1,5 +1,3 @@
-import random
-
 def Encryption(string, operation):
     new_string = ""
     for i in range(len(string)):
@@ -9,16 +7,16 @@ def Encryption(string, operation):
 def Decryption(string, operation):
     old_string = ''
     for i in range(len(string)):
-       old_string += chr(int(ord(new_string[i]) - operation))
+       old_string += chr(int(ord(string[i]) - operation))
     return old_string
 
 while(True):
     eord = input("Encryption or Decryption ")
-    if(eord == 'e' or 'encryption'):
+    if eord == 'e' or eord == 'encryption':
         string = input("what do you want to encrypt? ")
         operation = int(input("What is the seed? ")) % 128
         print(Encryption(string, operation))
-    elif(eord == 'd' or 'decryption'):
+    elif eord == 'd' or eord == 'decryption':
         string = input("what do you want to decrypt? ")
         operation = int(input("What is the seed? ")) % 128
         print(Decryption(string, operation))
